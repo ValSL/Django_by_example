839c992cbdd14556aa5f2522d0ccfd20f41e1ba3
valid <valid.saley@gmail.com>
2020-05-09 18:53:23 +0300

    User registration

--- Social Website/bookmarks/account/urls.py
+++ Social Website/bookmarks/account/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     # path('reset/done', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     # or
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register')
 ]
