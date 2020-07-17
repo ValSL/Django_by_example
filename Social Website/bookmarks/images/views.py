70882ce601d7c7162e60e95d4b5681fbb4316a93
valid <valid.saley@gmail.com>
2020-07-17 21:54:38 +0300

    Adding AJAX pagination

--- Social Website/bookmarks/images/views.py
+++ Social Website/bookmarks/images/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 @login_required
@@ -27,6 +29,7 @@ def image_detail(request, id, slug):
     return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
 
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
@@ -43,3 +46,23 @@ def image_like(request):
         except:
             pass
         return JsonResponse({'status': 'error'})
+
+
+@login_required
+def images_list(request):
+    image = Image.objects.all()
+    paginator = Paginator(image, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+
+    if request.is_ajax():
+        return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+
