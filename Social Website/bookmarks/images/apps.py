02d1d4b1936ee93603dc2e05190fdffd29f87d4d
valid <valid.saley@gmail.com>
2020-07-27 17:35:04 +0300

    Using signals for denormalizing counts

--- Social Website/bookmarks/images/apps.py
+++ Social Website/bookmarks/images/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ImagesConfig(AppConfig):
     name = 'images'
+
+    # Функция ready() требуется для регистрации сигналов
+    def ready(self):
+        import images.signals
